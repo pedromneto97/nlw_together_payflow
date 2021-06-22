8e03573160017f5616f218e3562e6c288064c0a4
Pedro Neto <pedromneto97@gmail.com>
2021-06-22 18:21:53 -0300

    feat: add splash screen

---
+++ lib/modules/splash/splash_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../../shared/themes/images.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: Image.asset(Images.union),
+          ),
+          Center(
+            child: Image.asset(Images.logoFull),
+          ),
+        ],
+      ),
+    );
+  }
+}
