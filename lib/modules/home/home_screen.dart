0b2d7bfedce36e08d7008e40e3af44a1cca586bd
Pedro Neto <pedromneto97@gmail.com>
2021-06-23 19:46:09 -0300

    feat: ajusta home screen

--- lib/modules/home/home_screen.dart
+++ lib/modules/home/home_screen.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'widgets/bottom_app_bar.dart';
+import 'widgets/home_app_bar.dart';
+
+class HomeScreen extends StatefulWidget {
   static const screenName = 'home_screen';
 
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+
+  void onChangeIndex(int index) {
+    if (index != currentIndex) {
+      setState(() {
+        currentIndex = index;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final items = [
+      Center(
+        child: Container(
+          color: Colors.red,
+        ),
+      ),
+      Center(
+        child: Container(
+          color: Colors.blue,
+        ),
+      )
+    ];
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeScreen'),
+      appBar: const HomeAppBar(),
+      body: items.elementAt(currentIndex),
+      bottomNavigationBar: PayflowBottomAppBar(
+        index: currentIndex,
+        onChangeIndex: onChangeIndex,
+        onTapAdd: () {},
       ),
     );
   }
