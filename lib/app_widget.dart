d3dce642286add03c28cb8825a14f491cd061d4b
Pedro Neto <pedromneto97@gmail.com>
2021-06-23 19:01:02 -0300

    feat: ajusta bloc e firebase

---
+++ lib/app_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'modules/home/home_screen.dart';
+import 'modules/login/login_screen.dart';
+import 'modules/splash/splash_screen.dart';
+import 'navigation_service.dart';
+import 'shared/auth/auth_bloc.dart';
+import 'shared/themes/colors.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => AuthBloc(),
+      child: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is SuccessAuthState) {
+            NavigationService().navigator.pushNamedAndRemoveUntil(
+                  HomeScreen.screenName,
+                  (route) => route.settings.name == '/',
+                );
+          } else if (state is InitialAuthState) {
+            NavigationService().navigator.pushNamedAndRemoveUntil(
+                  LoginScreen.screenName,
+                  (route) => route.settings.name == '/',
+                );
+          }
+        },
+        child: MaterialApp(
+          title: 'Payflow',
+          theme: ThemeData(
+            primaryColor: AppColors.primary,
+            scaffoldBackgroundColor: AppColors.background,
+          ),
+          initialRoute: SplashScreen.screenName,
+          routes: {
+            SplashScreen.screenName: (context) => const SplashScreen(),
+            LoginScreen.screenName: (context) => const LoginScreen(),
+            HomeScreen.screenName: (context) => const HomeScreen(),
+          },
+          navigatorKey: NavigationService().navigatorKey,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,60 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-import 'modules/login/login_screen.dart';
-import 'shared/themes/colors.dart';
+import 'app_widget.dart';
+import 'log_bloc_observer.dart';
 
-void main() {
-  runApp(AppWidget());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
+  if (kDebugMode) Bloc.observer = LogBlocObserver();
+  runApp(const AppFirebase());
 }
 
-class AppWidget extends StatelessWidget {
-  // This widget is the root of your application.
+class AppFirebase extends StatefulWidget {
+  const AppFirebase({Key? key}) : super(key: key);
+
+  @override
+  _AppFirebaseState createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-        scaffoldBackgroundColor: AppColors.background,
-      ),
-      home: const LoginScreen(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Material(
+            child: Center(
+              child: Text(
+                'Não foi possível inicializar o Firebase',
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+          );
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const AppWidget();
+        }
+
+        return const Material(
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
     );
   }
 }
