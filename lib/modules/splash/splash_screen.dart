15f5a9b7b79df6d8cc08ac12669f50b95f7a8f65
Pedro Neto <pedromneto97@gmail.com>
2021-06-23 19:01:59 -0300

    feat: ajusta navegação quando abre o app

--- lib/modules/splash/splash_screen.dart
+++ lib/modules/splash/splash_screen.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../shared/auth/auth_bloc.dart';
 import '../../shared/themes/images.dart';
+import '../home/home_screen.dart';
+import '../login/login_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   static const screenName = 'splash_screen';
 
   const SplashScreen({Key? key}) : super(key: key);
+
+  void onRender(BuildContext context, AuthState state) async {
+    await Future.delayed(
+      const Duration(seconds: 1),
+      () => Navigator.of(context).pushReplacementNamed(
+        state is SuccessAuthState
+            ? HomeScreen.screenName
+            : LoginScreen.screenName,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +33,15 @@ class SplashScreen extends StatelessWidget {
           Center(
             child: Image.asset(AppImages.logoFull),
           ),
+          BlocBuilder<AuthBloc, AuthState>(
+            builder: (context, state) {
+              WidgetsBinding.instance!.addPostFrameCallback(
+                (_) => onRender(context, state),
+              );
+
+              return const SizedBox();
+            },
+          )
         ],
       ),
     );
