1611453ee2dda208ee2141d44c8d06e031864b42
Pedro Neto <pedromneto97@gmail.com>
2021-06-24 22:27:54 -0300

    feat: adiciona flag de boarda

--- lib/shared/widgets/set_buttons/set_buttons.dart
+++ lib/shared/widgets/set_buttons/set_buttons.dart
@@ -3,12 +3,19 @@ import 'package:flutter/material.dart';
 import '../../themes/colors.dart';
 import '../../themes/text_styles.dart';
 
+enum Button {
+  primary,
+  secondary,
+  none,
+}
+
 class SetButtons extends StatelessWidget {
   final String primaryLabel;
   final VoidCallback primaryOnPressed;
   final String secondaryLabel;
   final VoidCallback secondaryOnPressed;
   final Button buttonPrimaryColor;
+  final bool hasBorderTop;
 
   const SetButtons({
     Key? key,
@@ -17,13 +24,23 @@ class SetButtons extends StatelessWidget {
     this.buttonPrimaryColor = Button.none,
     required this.secondaryLabel,
     required this.secondaryOnPressed,
+    this.hasBorderTop = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 56,
-      color: AppColors.shape,
+      decoration: BoxDecoration(
+        color: AppColors.shape,
+        border: hasBorderTop
+            ? const Border(
+                top: BorderSide(
+                  color: AppColors.stroke,
+                ),
+              )
+            : null,
+      ),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
